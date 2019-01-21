db3651fdd1240e89d339c37c86fe23a1f7eae137
zb <>
2019-01-21 16:04:24 +0800

    extract-text-webpack-plugin和optimize-css-assets-webpack-plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,6 +8,20 @@ const config = merge(common, {
     contentBase: './dist',
     hot: true,
   },
+  watch: true,
+  // 监听模式运行时的参数
+  // 在开启监听模式时，才有意义
+  watchOptions: {
+    // 不监听的文件或文件夹，支持正则匹配
+    // 默认为空
+    ignored: /node_modules/,
+    // 监听到变化发生后会等300ms再去执行动作，防止文件更新太快导致重新编译频率太高
+    // 默认为 300ms
+    aggregateTimeout: 300,
+    // 判断文件是否发生变化是通过不停的去询问系统指定文件有没有变化实现的
+    // 默认每秒问 1000 次
+    poll: 1000
+  },
   module: {
     rules: [
       {
@@ -19,12 +33,19 @@ const config = merge(common, {
         ],
       }, {
         test: /\.(sass|scss|css)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'resolve-url-loader',
-          'sass-loader?sourceMap',
-        ],
+        // use: [
+        //   'style-loader',
+        //   'css-loader',
+        //   'resolve-url-loader',
+        //   'sass-loader?sourceMap',
+        // ],
+        use: [{
+          loader: "style-loader" // 将 JS 字符串生成为 style 节点
+      }, {
+          loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+      }, {
+          loader: "sass-loader" // 将 Sass 编译成 CSS
+      }]
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,15 @@ const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const config = merge(common, {
   devtool: false,
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
   module: {
     rules: [
       {
@@ -13,14 +20,22 @@ const config = merge(common, {
           'babel-loader',
           'eslint-loader',
         ],
-      }, {
-        test: /\.(sass|scss|css)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'resolve-url-loader',
-          'sass-loader?sourceMap',
-        ],
+      }, 
+      // {
+      //   test: /\.(sass|scss|css)$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader',
+      //     'resolve-url-loader',
+      //     'sass-loader?sourceMap',
+      //   ],
+      // },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
@@ -32,6 +47,9 @@ const config = merge(common, {
   },
   plugins: [
     // new CleanWebpackPlugin(['./dist']),
+    new ExtractTextPlugin({
+      filename: `[name]_[md5:contenthash:hex:20].css`,// 给输出的 CSS 文件名称加上 Hash 值
+    }),
     new UglifyJSPlugin(),
   ],
 });
